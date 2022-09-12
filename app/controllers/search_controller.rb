c27ee9f3df1fd5af047ccb8394631f1f1b7d5294
Yui Sato <yui_sato@micin.jp>
2022-09-12 11:36:07 +0900

    update: 徒歩を追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,13 +4,15 @@ require 'uri'
 class SearchController < ApplicationController
     GEOCORDER_API_BASE_URL = 'https://msearch.gsi.go.jp/address-search/AddressSearch?'
     EXPRESS_API_BASE_URL = 'http://express.heartrails.com/api/json?'
+    METERS_PER_MINUTE = 80
 
     def getNearestStation
         address = params[:address]
         lng, lat = getCoordinateFromAddress(address)
-        station = getStationFromCoordinate(lng, lat)
+        station, distance = getStationFromCoordinate(lng, lat)
+        minutes = (distance.to_i.fdiv(METERS_PER_MINUTE)).ceil
 
-        render json: { status: 'SUCCESS', data: { station: station } }
+        render json: { status: 'SUCCESS', data: { station: station, minutes: minutes } }
     end
 
     # 住所から経度緯度を取得する
@@ -18,12 +20,7 @@ class SearchController < ApplicationController
         encodedQuery = URI.encode_www_form(q: address)
         geocorderUrl = GEOCORDER_API_BASE_URL + encodedQuery
         uri = URI.parse(geocorderUrl)
-
-        # TODO: リクエスト共通化
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri.request_uri)
-        http.use_ssl = true
-        response = http.request(request)    
+        response = getRequest(uri, true)
         return JSON.parse(response.body)[0]["geometry"]["coordinates"]
     end
 
@@ -32,12 +29,16 @@ class SearchController < ApplicationController
         encodedQuery = URI.encode_www_form(method: "getStations", x: lng, y: lat)
         expressUrl = EXPRESS_API_BASE_URL + encodedQuery
         uri = URI.parse(expressUrl)
+        response = JSON.parse(getRequest(uri).body)["response"]["station"][0]
+        return response["name"], response["distance"]
+    end
 
-        # TODO: リクエスト共通化
+    def getRequest(uri, isHttps = false)
         http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if isHttps
+
         request = Net::HTTP::Get.new(uri.request_uri)
-        response = http.request(request)
-    
-        return JSON.parse(response.body)["response"]["station"][0]["name"]
+        response = http.request(request)  
+        return response
     end
 end
